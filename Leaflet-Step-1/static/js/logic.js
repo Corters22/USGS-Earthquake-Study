de266f50010622c83c78843716269fb6ad699cc6
code_red <72528267+Corters22@users.noreply.github.com>
2021-04-28 17:48:35 -0500

    nothing changed

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -8,24 +8,28 @@ L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
     tileSize: 512,
     maxZoom: 18,
     zoomOffset: -1,
-    id: "light-v10",
+    id: "mapbox/streets-v11",
     accessToken: API_KEY
   }).addTo(myMap);
 
 var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson'
 
-var quakeMarkers = []
+d3.json(url).then(function(data) {
+    console.log('data', data)
+    var quakeMarkers = []
 
-for (i=0; i< data.features.length; i++) {
-    var lat = data.features[i].geometry.coordinates[1];
-    var long = data.features[i].geometry.coordinates[0];
-    quakeMarkers.push(
-        L.circle(lat, long, {
-            stroke: false,
-            fillOpacity: 0.75,
-            color: 'green',
-            fillColor: 'green',
-            radius: data.features[i].properties.mag
+    for (i=0; i< data.features.length; i++) {
+        var lat = data.features[i].geometry.coordinates[1];
+        var long = data.features[i].geometry.coordinates[0];
+        
+        quakeMarkers.push(
+            L.circle(lat, long, {
+                stroke: false,
+                fillOpacity: 0.75,
+                color: 'green',
+                fillColor: 'green',
+                radius: data.features[i].properties.mag
         })
     )
 }
+});
