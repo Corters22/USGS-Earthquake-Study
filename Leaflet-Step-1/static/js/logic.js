184b99c7af360b0a452d721ef7d1a6df8051ef72
code_red <72528267+Corters22@users.noreply.github.com>
2021-04-30 16:39:07 -0500

    added addtional base layers and legend

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -1,7 +1,7 @@
 var myMap = L.map('map', {
     center: [0, 0],
     zoom: 2,
-    layers: [quakeLayer]
+    
 })
 
 var streetLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -12,6 +12,28 @@ var streetLayer = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{
     id: "mapbox/streets-v11",
     accessToken: API_KEY
   }).addTo(myMap);
+
+var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
+    attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
+    tileSize: 512,
+    maxZoom: 18,
+    zoomOffset: -1,
+    id: "mapbox/satellite-v11",
+    accessToken: API_KEY
+  }).addTo(myMap);
+
+var Stamen_TerrainBackground = L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.{ext}', {
+	attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	subdomains: 'abcd',
+	minZoom: 0,
+	maxZoom: 18,
+	ext: 'png'
+});
+
+var sat = L.tileLayer('http://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}',{
+    maxZoom: 20,
+    subdomains:['mt0','mt1','mt2','mt3']
+});
 
 var url = 'https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/4.5_week.geojson'
 
@@ -44,9 +66,9 @@ d3.json(url).then(function(data) {
         var magnitude = feature.properties.mag;
         var location = feature.properties.place;
         var depth = feature.geometry.coordinates[2];
-        console.log('magnitude:', magnitude);
-        console.log('lat:', lat, 'long:', long);
-        console.log('depth', depth);
+        // console.log('magnitude:', magnitude);
+        // console.log('lat:', lat, 'long:', long);
+        // console.log('depth', depth);
         quakeMarkers.push(
             L.circle([lat, long], {
                 stroke: false,
@@ -54,7 +76,7 @@ d3.json(url).then(function(data) {
                 color: 'white',
                 fillColor: markerColor(depth),
                 radius: markerSize(magnitude)
-            }).bindPopup(location + "<br> Magnitude:" + magnitude))
+            }).bindPopup(location + "<br> Magnitude: " + magnitude))
         }
         // if (lat) {
         //     console.log(quakeMarkers)
@@ -66,9 +88,36 @@ d3.json(url).then(function(data) {
         Earthquakes: quakeLayer
     }
     var baseMaps = {
-        Street: streetLayer
+        Street: streetLayer, 
+        Satellite: sat, 
+        Terrain: Stamen_TerrainBackground
     }
-    L.control.layers(baseMaps, overlayMaps).addTo(myMap)
-    // L.geoJson(data).addTo(myMap);
+    L.control.layers(baseMaps, overlayMaps, {
+        collapsed: false
+    }).addTo(myMap)
+    
+    var legend = L.control({ postion: 'bottomright'});
+    legend.onAdd = function() {
+        var div = L.DomUtil.create('div', 'info legend');
+        var colors = ['greenyellow', 'yellow', 'gold', 'orange', 'orangered', 'red'];
+        var limits = ['-10-10', '10-30', '30-50', '50-70', '70-90', '90+'];
+        var labels = [];
 
+        var legendInfo = '<div class =\"labels\">' + '</div>';
+
+        div.innerHTML = legendInfo;
+
+        limits.forEach(function(limit, index) {
+            labels.push('<li style=\"background-color: ' + colors[index] + '\'>' + limits[index] + '</li>');
+        });
+
+        div.innerHTML += "<ul>" + labels.join("") + "</ul>";
+        return div;
+    };
+
+    legend.addTo(myMap);
 });
+
+//todo: create function to make map
+//create function to make markers
+//call functions with d3.json
